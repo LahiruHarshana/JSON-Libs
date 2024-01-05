df332feeaf88aca46416b8584ff3049cc541ab57
LahiruHarshana <lharshana2002@gmail.com>
2024-01-05 15:49:01 +0530

    updated

---
+++ src/main/java/ijse/lk/jsonlibraries/jsonlibraries/JsonProcessServlet.java
@@ -0,0 +1,22 @@
+package ijse.lk.jsonlibraries.jsonlibraries;
+
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "JsonProcessServlet", value = "/json")
+public class JsonProcessServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    }
+
+    public void destroy() {
+    }
+}

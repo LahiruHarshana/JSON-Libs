5173b593d288baf9197f02e7ce069c3beef0a043
LahiruHarshana <lharshana2002@gmail.com>
2024-01-12 10:34:57 +0530

    updated

--- src/main/java/ijse/lk/jsonlibraries/jsonlibraries/model/Student.java
+++ src/main/java/ijse/lk/jsonlibraries/jsonlibraries/model/Student.java
@@ -1,40 +1,12 @@
 package ijse.lk.jsonlibraries.jsonlibraries.model;
 
+
+import lombok.Data;
+
+@Data
 public class Student {
     private String id;
     private String name;
     private int age;
 
-    public Student() {
-    }
-
-    public Student(String id, String name, int age) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }

064cb823796d06376968b09b769f5dd5d89fad0f
LahiruHarshana <lharshana2002@gmail.com>
2024-01-12 10:27:02 +0530

    updated

--- src/main/java/ijse/lk/jsonlibraries/jsonlibraries/JsonBindServlet.java
+++ src/main/java/ijse/lk/jsonlibraries/jsonlibraries/JsonBindServlet.java
@@ -1,5 +1,9 @@
 package ijse.lk.jsonlibraries.jsonlibraries;
 
+
+import jakarta.servlet.annotation.WebServlet;
+
+@WebServlet(name = "JsonBindServlet", value = "/json-bind")
 public class JsonBindServlet {
 
 }

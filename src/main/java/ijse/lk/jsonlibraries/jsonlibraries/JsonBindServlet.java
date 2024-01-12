483eb364fed11b47b2b93303beb914fef53ddaf1
LahiruHarshana <lharshana2002@gmail.com>
2024-01-12 10:27:19 +0530

    updated

--- src/main/java/ijse/lk/jsonlibraries/jsonlibraries/JsonBindServlet.java
+++ src/main/java/ijse/lk/jsonlibraries/jsonlibraries/JsonBindServlet.java
@@ -4,6 +4,15 @@ package ijse.lk.jsonlibraries.jsonlibraries;
 import jakarta.servlet.annotation.WebServlet;
 
 @WebServlet(name = "JsonBindServlet", value = "/json-bind")
-public class JsonBindServlet {
+public class JsonBindServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    }
 
 }

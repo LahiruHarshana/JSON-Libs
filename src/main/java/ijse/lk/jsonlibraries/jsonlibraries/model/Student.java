3ce80cb171f35c4253c999123b1e3236361d8614
LahiruHarshana <lharshana2002@gmail.com>
2024-01-12 10:37:26 +0530

    updated

--- src/main/java/ijse/lk/jsonlibraries/jsonlibraries/model/Student.java
+++ src/main/java/ijse/lk/jsonlibraries/jsonlibraries/model/Student.java
@@ -3,9 +3,11 @@ package ijse.lk.jsonlibraries.jsonlibraries.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Student {
     private String id;
     private String name;
